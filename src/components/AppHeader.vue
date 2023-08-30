b9f8b853ec2d6640927facd179dce91ea1207932
Faisal ahmed <faisal2410@yahoo.com>
2023-08-30 14:46:21 +0600

    updated

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useModalStore } from '../stores/modal'
 import {useUserStore} from '../stores/user.js'
+import { RouterLink } from 'vue-router'
 
 const modalStore = useModalStore()
 const userStore=useUserStore()
@@ -11,20 +12,25 @@ const userStore=useUserStore()
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link class="text-white font-bold uppercase text-2xl mr-4" :to="{ name: 'home' }">Music</router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }"
+              >About</router-link>
+          </li>
+          
+
           <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="modalStore.toggleAuthModal"
-              >Login / Register</a
-            >
+              >Login / Register</a>
           </li>
           <template v-else>
            <li >
-            <a class="px-2 text-white" href="#">Manage</a>
+            <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
           </li>
            <li >
             <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
@@ -37,4 +43,4 @@ const userStore=useUserStore()
   </header>
 </template>
 
-<style scoped></style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from "../views/HomeView.vue";
+import About from "../views/AboutView.vue";
+import Manage from "../views/ManageView.vue";
 
 
-const routes = [];
+const routes = [
+  {
+    name: "home",
+    path: "/",
+    component: Home,
+  },
+  {
+    name: "about",
+    path: "/about",
+    component: About,
+  },
+  {
+    name: "manage",    
+    path:"/manage",
+    component: Manage,   
+  }, 
+  {
+    path: "/:catchAll(.*)*",
+    redirect: { name: "home" },
+  },
+
+
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  linkExactActiveClass: "text-yellow-500",
   
 })
+
+
 
 export default router
